c1d27f6903c0b2ac42ca22faf172c7bbce3f1465
Dobrosław Żybort <matrixik@gmail.com>
2013-01-18 18:14:44 +0100

    Update example

--- examples/github.go
+++ examples/github.go
@@ -66,15 +66,17 @@ func main() {
 	//github.AccessTokenInURL = true
 
 	// Make the request.
-	r, err := github.Get("user")
+	// Provide API end point (http://developer.github.com/v3/users/#get-the-authenticated-user)
+	apiEndPoint := "user"
+	githubUserData, err := github.Get(apiEndPoint)
 	if err != nil {
 		log.Fatal("Get:", err)
 	}
-	defer r.Body.Close()
+	defer githubUserData.Body.Close()
 
 	fmt.Println("User info:")
 	// Write the response to standard output.
-	io.Copy(os.Stdout, r.Body)
+	io.Copy(os.Stdout, githubUserData.Body)
 
 	fmt.Println()
 }
